cb58ff3a7217a17ed673e44abb7f77eec48402e0
Serhii Nechytailo <Serhii.Nechytailo@myhomepay.com>
2019-11-18 12:12:46 +0200

    Console FFFF

---
+++ ConsoleStudy/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKey key;
+            while (true)
+            {
+                key = Console.ReadKey().Key;
+                if (key == ConsoleKey.F)
+                {
+                    Console.WriteLine("FFFFFFFF");
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
